95919d3d33fe6dd5d362bccdbbfa589bf11cf0b6
Loris SOGLIUZZO <loris@sogliuzzo.com>
2026-02-06 16:35:20 +0100

    Refactor game loading logic in test.py to improve readability and maintainability

--- src/test.py
+++ src/test.py
@@ -1,4 +1,3 @@
-# %%
 import os
 
 import chess.pgn
@@ -6,27 +5,29 @@ import chess.pgn
 from fetch_games import DATA_FOLDER
 
 player_id = "15940"  # Kasparov's player ID
-pgn_path = os.path.join('..', DATA_FOLDER, player_id)
-game = None
-board = None
+pgn_path = os.path.join(DATA_FOLDER, player_id)
+first = True
+game_count = 0
+
 for filename in os.listdir(pgn_path):
     if not filename.endswith(".pgn"):
         continue
 
-    file_path = os.path.join(pgn_path, filename)
-    with open(file_path, "r", encoding="utf-8") as pgn_file:
-        game = chess.pgn.read_game(pgn_file)
-        print(f"Game: {game.headers.get('Event', 'N/A')}")  # type: ignore
-        print(f"White: {game.headers.get('White', 'N/A')}")  # type: ignore
-        print(f"Black: {game.headers.get('Black', 'N/A')}")  # type: ignore
-        print(f"Result: {game.headers.get('Result', 'N/A')}")  # type: ignore
-        print(f"Date: {game.headers.get('Date', 'N/A')}\n")  # type: ignore
-        board = game.board()  # type: ignore
-        for move in game.mainline_moves():  # type: ignore
-            board.push(move)
+    game_count += 1
 
-        break
+    if first:
+        file_path = os.path.join(pgn_path, filename)
+        with open(file_path, "r", encoding="utf-8") as pgn_file:
+            game = chess.pgn.read_game(pgn_file)
+            print(f"Game: {game.headers.get('Event', 'N/A')}")
+            print(f"White: {game.headers.get('White', 'N/A')}")
+            print(f"Black: {game.headers.get('Black', 'N/A')}")
+            print(f"Result: {game.headers.get('Result', 'N/A')}")
+            print(f"Date: {game.headers.get('Date', 'N/A')}\n")
+            board = game.board()
+            for move in game.mainline_moves():
+                board.push(move)
+            print(board)
+        first = False
 
-board  # type: ignore
-
-# %%
+print(f"Total games: {game_count}")
