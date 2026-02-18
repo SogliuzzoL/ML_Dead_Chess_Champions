321e3c95316508abcfc8a314e688f23128861499
Loris Sogliuzzo <loris@sogliuzzo.com>
2026-02-18 09:50:26 +0100

    Update .gitignore and requirements; enhance clustering script with UMAP integration

--- src/cluster.py
+++ src/cluster.py
@@ -1,14 +1,31 @@
+import os
+
 import numpy as np
-import umap
+import pandas as pd
+from cuml import UMAP
 from sklearn.model_selection import train_test_split
 
-from core.config import MAIA_EMBEDDINGS_PATH, logger
+from core.config import DATA_FOLDER, DATASET_PATH, MAIA_EMBEDDINGS_PATH, logger
 
-embeddings = np.load(MAIA_EMBEDDINGS_PATH)
+if __name__ == "__main__":
+    embeddings = np.load(MAIA_EMBEDDINGS_PATH, mmap_mode='r')
+    df = pd.read_parquet(DATASET_PATH)
+    logger.info("Loaded %d embeddings", len(embeddings))
 
-train, test = train_test_split(embeddings, test_size=0.2, random_state=42)
+    assert len(embeddings) == len(
+        df), "Number of embeddings does not match number of records in the dataset"
 
-umap_model = umap.UMAP(n_neighbors=15, min_dist=0.1,
-                       n_components=2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        embeddings, df["player_name"], test_size=0.2)
 
-umap_embeddings = umap_model.fit_transform(train)
+    umap_model = UMAP(n_components=2)
+    logger.info("Fitting UMAP model to training data")
+    umap_model.fit(X_train)
+
+    logger.info("Transforming test data using UMAP model")
+    X_test_umap = umap_model.transform(X_test)
+
+    test_umap_df = pd.DataFrame(X_test_umap, columns=["UMAP1", "UMAP2"])
+    test_umap_df["player_name"] = y_test.values
+
+    test_umap_df.to_parquet(os.path.join(DATA_FOLDER, "test_umap.parquet"))
